8295cd919e53490496b2b538973ba49733df5beb
Jarlaxle1979 <jarlaxle1979@outlook.com>
2016-12-19 17:24:43 +0000

    Created head include file

---
+++ inc/head.php
@@ -0,0 +1,6 @@
+<head>
+	<title>Free Game Reviews</title>
+	<!--Styles-->
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/gallergy.css">
+</head>
